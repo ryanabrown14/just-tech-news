c2dc56e5ecf5e46068284257ae2b62964d41db57
ry brown <ryan.alex.brown@gmail.com>
2020-10-29 15:49:42 -0700

    login/logout

--- server.js
+++ server.js
@@ -4,11 +4,29 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
+const session = require('express-session');
+
+
+
+
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+app.use(session(sess));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -16,6 +34,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 // turn on routes
 app.use(routes);
+
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
