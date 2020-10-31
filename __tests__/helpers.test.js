18f6e8154cce2c22cc9acec32374d843a3a23541
ry brown <ryan.alex.brown@gmail.com>
2020-10-31 00:48:31 -0700

    formating

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,26 @@
+const {format_date} = require('../utils/helpers');
+const {format_url} = require('../utils/helpers');
+const {format_plural} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+  
+    expect(format_date(date)).toBe('3/20/2020');
+  });
+
+  test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+  
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
+  });
+  test('format_plural() returns to correct pluralization', () => {
+      const tiger1 = format_plural("Tiger", 1);
+      const tiger2= format_plural("Tiger", 2);
+
+      expect(tiger1).toBe('Tiger');
+      expect(tiger2).toBe('Tigers');
+  })
--- server.js
+++ server.js
@@ -1,10 +1,12 @@
+const helpers = require('./utils/helpers');
 const path = require('path');
 const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
+
 
 
 
