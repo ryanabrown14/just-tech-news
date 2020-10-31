f7047bf9f96f0379643d13b423c297ed0a16b3b0
ry brown <ryan.alex.brown@gmail.com>
2020-10-30 17:42:16 -0700

    login/logout/singlepost

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -79,13 +79,17 @@ router.get('/', (req, res) => {
   });
 
   router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
-    .catch(err => {
-      console.log(err);
-      res.status(400).json(err);
-    });
+    //make sure the session exists first
+    if(req.session) {
+      //pass session id along with all destructured properties
+      Post.upvote({...req.body, user_id: req.session.user_id}, {Vote, Comment, User})
+      .then(updatedVoteData => res.json(updatedVoteData))
+      .catch(err => {
+        console.log(err);
+        res.status(400).json(err);
+      });
+    }
+
 });
 
 
